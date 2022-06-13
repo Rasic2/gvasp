30048caf685134ca5d52cb77ed2bd4e4d16fb3a1
Rasic2 <1051987201@qq.com>
2022-06-13 12:41:53 +0800

    reorganize  &  directory

--- Band/band.sh
+++ Band/band.sh
@@ -1,4 +1,6 @@
-bandn=$(sed -n '6p' EIGENVAL | gawk '{print $3}')
+#!/usr/bin/bash
+
+bandn=`sed -n '6p' EIGENVAL | gawk '{print $3}'`
 for (( i = 1; i <= $bandn; i++ ))
 	do
 		gawk "/^\s+${i}\s+-?[0-9]+\./{print $1}" EIGENVAL | gawk '{printf "%10s\n",($2+$3)/2}' >> $i.log
