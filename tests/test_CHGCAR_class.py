e2157a4563e08716d16108e044a43fa01ccd71af
Rasic2 <1051987201@qq.com>
2022-07-20 22:33:55 +0800

    add chgsplit in <CHGCAR class>

--- tests/test_CHGCAR_class.py
+++ tests/test_CHGCAR_class.py
@@ -1,4 +1,4 @@
-from file import CHGCAR_tot, CHGCAR_mag
+from file import CHGCAR_tot, CHGCAR_mag, CHGCAR
 
 chg_tot = CHGCAR_tot("CHGCAR_tot")
 chg_tot.load()
@@ -6,4 +6,6 @@ chg_tot.load()
 chg_mag = CHGCAR_mag("CHGCAR_mag")
 chg_mag.load()
 
+chg = CHGCAR("CHGCAR")
+chg.split()
 print()
