a4f908660616b2e7241687938df911f93e51b799
Rasic2 <1051987201@qq.com>
2022-08-07 00:29:14 +0800

    modify the ssetup.py to retry construct docs

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
+from setuptools import setup, find_packages
 from distutils.extension import Extension
 from pathlib import Path
 
 from Cython.Build import cythonize
-from setuptools import setup, find_packages
 
 setup(
     name='QVasp',
@@ -27,8 +27,7 @@ setup(
     ext_modules=cythonize([Extension(name='QVasp.lib._dos', sources=['extension/_dos/_dos.pyx']),
                            Extension(name='QVasp.lib._file', sources=['extension/_file/_file.cpp',
                                                                       'extension/_file/_lib.cpp'])], language_level=3),
-    include_dirs=['/usr/lib/gcc/x86_64-linux-gnu/11/include', '/home/hzhou/anaconda3/include',
-                  '/home/hzhou/anaconda3/Library/include'],
+    include_dirs=['/home/hzhou/anaconda3/include', '/home/hzhou/anaconda3/Library/include'],
     include_package_data=True,
     package_data={"QVasp": ["*.json", "*.yaml", "INCAR", "pot.tgz"]},
     entry_points={'console_scripts': ['QVasp = QVasp.main:main']}
