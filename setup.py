3a28924b9e22e1b911b0e973e4f4e5defdf40ce9
Rasic2 <1051987201@qq.com>
2022-08-07 09:27:45 +0800

    use function to get the include path

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
+import sysconfig
 from setuptools import setup, find_packages
 from distutils.extension import Extension
 from pathlib import Path
 
+import pybind11
 from Cython.Build import cythonize
 
 setup(
-    name='QVasp',
-    description='A quick post-process for resolve or assistant the VASP calculations',
-    author='hui_zhou',
-    long_description=Path("./README.md").read_text(),
-    long_description_content_type='text/markdown',
+    name='gvasp',
     version='0.0.1',
     license='GPL-3.0',
+    author='hui_zhou',
+    author_email='1051987201@qq.com',
+    url='https://github.com/Rasic2/gvasp',
+    description='A quick post-process for resolve or assistant the VASP calculations',
+    long_description=Path("./README.md").read_text(),
+    long_description_content_type='text/markdown',
     python_requires='>=3.9',
     packages=find_packages(),
+    setup_requires=[
+        'pybind11',
+        'Cython'
+    ],
     install_requires=[
+        'pybind11',
         'Cython',
         'lxml',
         'matplotlib',
@@ -27,7 +36,7 @@ setup(
     ext_modules=cythonize([Extension(name='QVasp.lib._dos', sources=['extension/_dos/_dos.pyx']),
                            Extension(name='QVasp.lib._file', sources=['extension/_file/_file.cpp',
                                                                       'extension/_file/_lib.cpp'])], language_level=3),
-    include_dirs=['/home/hzhou/anaconda3/include', '/home/hzhou/anaconda3/Library/include'],
+    include_dirs=[sysconfig.get_config_var("INCLUDE"), pybind11.get_include()],
     include_package_data=True,
     package_data={"QVasp": ["*.json", "*.yaml", "INCAR", "pot.tgz"]},
     entry_points={'console_scripts': ['QVasp = QVasp.main:main']}
