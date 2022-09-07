27f51b3326665324efbe0eb8e5731d2e3bd2a2d0
Rasic2 <1051987201@qq.com>
2022-09-07 23:43:30 +0800

    add test_structure in codecov

---
+++ tests/test_structure.py
@@ -0,0 +1,15 @@
+import unittest
+from pathlib import Path
+
+from gvasp.common.setting import RootDir
+from gvasp.common.file import POSCAR
+
+
+class TestStructure(unittest.TestCase):
+    def test_structure(self):
+        structure = POSCAR(f"{Path(RootDir).parent}/tests/POSCAR_IS_sort").structure
+        structure.find_neighbour_table(neighbour_num=structure.atoms.count, sort=False, including_self=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
