005333ff5d11ab08121cc37e78ff768351c0b530
Rasic2 <1051987201@qq.com>
2022-09-07 22:30:10 +0800

    test codecov

--- tests/test_align.py
+++ tests/test_align.py
@@ -2,12 +2,13 @@ import os
 import unittest
 from pathlib import Path
 
+from gvasp.common.setting import RootDir
 from gvasp.common.file import POSCAR
 
 
 class TestAlign(unittest.TestCase):
     def test_align(self):
-        POSCAR.align("POSCAR_IS", "POSCAR_FS")
+        POSCAR.align(f"{Path(RootDir).parent}/tests/POSCAR_IS", f"{Path(RootDir).parent}/tests/POSCAR_FS")
         for file in Path(".").glob("POSCAR_*_sort"):
             os.remove(file)
 
