46f3b9cd214ebc63979a579096d34a43e0f5fb43
Rasic2 <1051987201@qq.com>
2022-09-08 14:26:33 +0800

    add test_submit in codecov

--- tests/modify_config.py
+++ tests/modify_config.py
@@ -1,9 +1,7 @@
 import json
 from pathlib import Path
 
-from gvasp.common.setting import RootDir
+config = {"potdir": f"{Path('.').absolute()}/tests/pot"}
 
-config = {"potdir": f"{Path(RootDir).parent}/tests/pot"}
-
-with open(f"{Path(RootDir)}/config.json", "w") as f:
+with open(f"{Path('.').absolute()}/gvasp/config.json", "w") as f:
     json.dump(config, f)
