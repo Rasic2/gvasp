e70245319f9d9d97114ff7df41a8bf4043b986d9
Rasic2 <1051987201@qq.com>
2022-09-08 15:15:54 +0800

    add test_output in codecov

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,3 +1,4 @@
+import shutil
 import unittest
 from pathlib import Path
 
@@ -7,6 +8,7 @@ from gvasp.common.file import XSDFile
 class TestOutput(unittest.TestCase):
     def test_output(self):
         XSDFile.write(contcar=Path(".") / "tests" / "CONTCAR", outcar=Path(".") / "tests" / "OUTCAR")
+        shutil.rmtree("output.xsd")
 
 
 if __name__ == '__main__':
