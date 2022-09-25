21a6724fbe4dc7d8707f0d4044a1dbff7f159ef3
Rasic2 <1051987201@qq.com>
2022-09-25 15:22:16 +0800

    update gvasp-complete

--- gvasp/gvasp-bash-completion.sh
+++ gvasp/gvasp-bash-completion.sh
@@ -3,22 +3,36 @@
 RED="\033[1;31m"
 RESET="\033[0m"
 
-_files () {
-    local c=0
-    for file in `ls`
-    do
-        files[$c]=$file
-        c=$((++c))
-    done
-    echo ${files[@]}
-}
-
 _gvasp_submit_opt() {
     local cur opts
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -l --low"
+    
+    allopts_short=(-P -V -S -C -l)
+    allopts_long=(--potential --vdw --sol --continuous --low)
+    allopts="-P --potential -V --vdw -S --sol -C --continuous -l --low"
+
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts="$allopts"
+    else
+        opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -l --low"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
 }
 
@@ -26,8 +40,38 @@ _gvasp_submit_chg() {
     local cur opts
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential -a --analysis"
+    
+    if [[ "${COMP_WORDS[@]}" =~ "-s" || "${COMP_WORDS[@]}" =~ "--sequential" ]]; then
+        allopts_short=(-P -s -V -S -C -a -l)
+        allopts_long=(--potential --sequential --vdw --sol --continuous --analysis --low)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous -a --analysis -l --low"
+    else
+        allopts_short=(-P -s -V -S -C -a)
+        allopts_long=(--potential --sequential --vdw --sol --continuous --analysis)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous -a --analysis"
+    fi
+    
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts="$allopts"
+    else
+        opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential -a --analysis"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
 }
 
@@ -35,10 +79,35 @@ _gvasp_submit_wf() {
     local cur opts
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
 
-    if [[ "${COMP_WORDS[@]}"  =~ "-s" || "${COMP_WORDS[@]}"  =~ "--sequential" ]]; then
-        opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential -l --low"
+    if [[ "${COMP_WORDS[@]}" =~ "-s" || "${COMP_WORDS[@]}" =~ "--sequential" ]]; then
+        allopts_short=(-P -s -V -S -C -l)
+        allopts_long=(--potential --sequential --vdw --sol --continuous --low)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous -l --low"
+    else
+        allopts_short=(-P -s -V -S -C)
+        allopts_long=(--potential --sequential --vdw --sol --continuous)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous"
+    fi
+    
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts="$allopts"
     else
         opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential"
     fi
@@ -49,10 +118,35 @@ _gvasp_submit_dos() {
     local cur opts
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
 
-    if [[ "${COMP_WORDS[@]}"  =~ "-s" || "${COMP_WORDS[@]}"  =~ "--sequential" ]]; then
-        opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential -l --low -a --analysis"
+    if [[ "${COMP_WORDS[@]}" =~ "-s" || "${COMP_WORDS[@]}" =~ "--sequential" ]]; then
+        allopts_short=(-P -s -V -S -C -l -a)
+        allopts_long=(--potential --sequential --vdw --sol --continuous --low --analysis)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous -l --low -a --analysis"
+    else
+        allopts_short=(-P -s -V -S -C)
+        allopts_long=(--potential --sequential --vdw --sol --continuous)
+        allopts="-P --potential -s --sequential -V --vdw -S --sol -C --continuous"
+    fi
+    
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts="$allopts"
     else
         opts="-h --help -P --potential -V --vdw -S --sol -C --continuous -s --sequential"
     fi
@@ -60,20 +154,77 @@ _gvasp_submit_dos() {
 }
 
 _gvasp_submit_neb() {
-    local cur opts
+    local cur opts fileshow=""
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -P --potential -V --vdw -S --sol -ini --ini_poscar -fni --fni_poscar -i --images -m --method -c --cancel_check_overlap"
-    COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
+
+    allopts_short=(-P -V -S -ini -fni -i -m -c)
+    allopts_long=(--potential --vdw --sol --ini_poscar --fni_poscar --images --method --cancel_check_overlap)
+    allopts="-P --potential -V --vdw -S --sol -ini --ini_poscar -fni --fni_poscar -i --images -m --method -c --cancel_check_overlap"
+    
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" || "$pre" =~ "-i" || "$pre" =~ "--images" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-ini" || "$pre" =~ "-fni" || "$pre" =~ "--ini_poscar" || "$pre" =~ "--fni_poscar" ]]; then
+        opts=""
+        fileshow="-o default"
+    elif [[ "$pre" =~ "-m" || "$pre" =~ "--method" ]]; then
+        opts="linear idpp"
+    elif [[ "$ppre" =~ "-ini" || "$ppre" =~ "--ini_poscar" ]]; then
+        opts="-fni --fni_poscar"    
+    elif [[ "$pre" =~ "-" || "$ppre" =~ "-" ]]; then
+        opts="$allopts"
+    else
+        opts="-h --help -P --potential -V --vdw -S --sol -ini --ini_poscar -fni --fni_poscar -i --images -m --method -c --cancel_check_overlap"
+    fi
+    COMPREPLY=( $( compgen $fileshow -W "$opts" -- $cur ) )
 }
 
 _gvasp_submit_normal() {
     local cur opts
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -P --potential -V --vdw -S --sol"
+
+    allopts_short=(-P -V -S)
+    allopts_long=(--potential --vdw --sol)
+    allopts="-P --potential -V --vdw -S --sol"
+    
+    for (( i=0; i<${#allopts_short[@]}; i++ ))
+    do
+        if [[ "${COMP_WORDS[@]}" =~ "${allopts_short[i]}" || "${COMP_WORDS[@]}" =~ "${allopts_long[i]}" ]]; then
+            allopts=${allopts/${allopts_short[i]}}
+            allopts=${allopts/${allopts_long[i]}}
+        fi
+    done
+
+    if [[ "$pre" =~ "-h" || "$pre" =~ "--help" ]]; then
+        opts=""
+    elif [[ "$pre" =~ "-P" || "$pre" =~ "--potential" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91"
+    elif [[ "$pre" =~ "PAW" || "$pre" =~ "USPP" ]]; then
+        opts="PAW_LDA PAW_PBE PAW_PW91 USPP_LDA USPP_PW91 $allopts"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts="$allopts"
+    else
+        opts="-h --help -P --potential -V --vdw -S --sol"
+    fi
+    
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
 }
 
@@ -81,84 +232,205 @@ _gvasp_submit() {
     local cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="opt con-TS chg wf dos freq md stm neb dimer -h --help"
+    if [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="opt con-TS chg wf dos freq md stm neb dimer -h --help"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
 _gvasp_movie() {
-    local cur opts 
+    local pre cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="opt con-TS freq md neb dimer -h --help"
+    if [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="opt con-TS freq md neb dimer -h --help"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
-_gvasp_movie_normal() {
-    local cur opts 
+_gvasp_movie_normal() { # gvasp movie [opt con-TS md dimer] completion
+    local pre cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-n --name"
+    if [ $COMP_CWORD -ge 5 ];then
+        echo -e "\n${RED}One arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="-n --name"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
-_gvasp_movie_freq() {
-    local cur opts 
+_gvasp_movie_freq() { # gvasp movie freq completion
+    local pre cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-n --name -f --freq"
+
+    if [ $COMP_CWORD -ge 7 ];then
+        echo -e "\n${RED}Two arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    elif [[ "$ppre" =~ "-f" || "$ppre" =~ "--freq" ]]; then
+        opts="-n --name"
+    elif [[ "$ppre" =~ "-n" || "$ppre" =~ "--name" ]]; then
+        opts="-f --freq"
+    else
+        opts="-n --name -f --freq"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
-_gvasp_movie_neb() {
-    local cur opts 
+_gvasp_movie_neb() { # gvasp movie neb completion
+    local pre cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-n --name -p --pos"
+
+    if [ $COMP_CWORD -ge 7 ];then
+        echo -e "\n${RED}Two arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-p" || "$pre" =~ "--pos" ]]; then
+        opts="POSCAR CONTCAR"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    elif [[ "$ppre" =~ "-p" || "$ppre" =~ "--pos" ]]; then
+        opts="-n --name"
+    elif [[ "$ppre" =~ "-n" || "$ppre" =~ "--name" ]]; then
+        opts="-p --pos"
+    else
+        opts="-n --name -p --pos"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
+}
+
+_gvasp_plot() {
+    local pre cur opts
+
+    COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    cur=${COMP_WORDS[COMP_CWORD]}
+    if [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="-h --help opt band ep dos PES neb"
+    fi
+    COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
+}
+
+_gvasp_plot_normal() { # gvasp plot [opt, band, op, dos, PES, neb]
+    local pre cur opts fileshow=""
+
+    COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
+    cur=${COMP_WORDS[COMP_CWORD]}
+    
+    if [[ "$pre" =~ "-j" || "$pre" =~ "--json" ]]; then
+        opts=""
+        fileshow="-o default"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    elif [[ "$ppre" =~ "-j" || "$ppre" =~ "--json" ]]; then
+        opts="--save --show"
+    else
+        opts="-h --help -j --json --show --save"
+    fi
+    COMPREPLY=( $( compgen $fileshow -W "$opts" -- $cur ) ) 
 }
 
 _gvasp_sort() {
-    local cur opts 
+    local pre cur opts fileshow=""
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -ini --ini_poscar -fni --fni_poscar"
-    COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
+    
+    if [[ $COMP_CWORD -ge 6 ]]; then
+        echo -e "\n${RED}Two arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-ini" || "$pre" =~ "-fni" || "$pre" =~ "--ini_poscar" || "$pre" =~ "--fni_poscar" ]]; then
+        opts=""
+        fileshow="-o default"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    elif [[ "$ppre" =~ "-ini" || "$ppre" =~ "--ini_poscar" ]]; then
+        opts="-fni --fni_poscar"
+    else
+        opts="-h --help -ini --ini_poscar -fni --fni_poscar"
+    fi
+    COMPREPLY=( $( compgen $fileshow -W "$opts" -- $cur ) ) 
 }
 
 _gvasp_split() {
     local cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help"
+    if [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="-h --help"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
-_gvasp_grd() {
-    local cur opts 
+_gvasp_grd() { # gvasp grd completion
+    local pre cur opts 
 
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    ppre=${COMP_WORDS[COMP_CWORD-2]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help -n --name -d --DenCut"
+
+    if [[ $COMP_CWORD -ge 6 ]]; then
+        echo -e "\n${RED}Two arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    elif [[ "$ppre" =~ "-d" || "$ppre" =~ "--DenCut" ]]; then
+        opts="-n --name"
+    elif [[ "$ppre" =~ "-n" || "$ppre" =~ "--name" ]]; then
+        opts="-d --DenCut"
+    else
+        opts="-h --help -n --name -d --DenCut"
+    fi
+
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) ) 
 }
 
 _gvasp_output() {
-    local cur opts
+    local pre cur opts
  
     COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="-h --help"
+    
+    if [[ $COMP_CWORD -ge 3 ]]; then
+        echo -e "\n${RED}One arguments maximum!${RESET}"
+    elif [[ "$pre" =~ "-" ]]; then
+        opts=""
+    else
+        opts="-h --help"
+    fi
     COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
 }
 
-_gvasp_config() {
+_gvasp_config() { # gvasp config completion
     local cur opts
  
     COMPREPLY=()
@@ -169,7 +441,7 @@ _gvasp_config() {
         opts=""
         COMPREPLY=( $( compgen -o default -W "$opts" -- $cur ) )
     elif [ $COMP_CWORD -ge 4 ];then
-        echo -e "\n${RED}Only support one argument!${RESET}"
+        echo -e "\n${RED}One argument maximum!${RESET}"
     else 
         opts="-h --help -f --file"
         COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
@@ -198,16 +470,14 @@ _gvasp() {
             command=$first_command
         fi
     fi
-
-    # declare -p command
     
-    if [ -z "$command" ];then # gvasp command completion
+    if [ -z "$command" ];then  # gvasp command completion
         COMPREPLY=()
         cur=${COMP_WORDS[COMP_CWORD]}
         opts="config submit output movie sort plot sum split grd -h --help -v --version -l --list "
         COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
     else
-        case "$command" in
+        case "$command" in  # gvasp subcommand completion
         config) _gvasp_config ;;
         submit) _gvasp_submit ;;
         submit-opt) _gvasp_submit_opt ;;
@@ -224,6 +494,8 @@ _gvasp() {
         sort) _gvasp_sort ;;
         sum | split) _gvasp_split ;;
         grd) _gvasp_grd ;;
+        plot) _gvasp_plot ;;
+        plot-*) _gvasp_plot_normal ;;
         esac 
     fi
 }
