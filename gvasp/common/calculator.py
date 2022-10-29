496fe0062073c83e4df36b087ddbd20d7487278c
Rasic2 <1051987201@qq.com>
2022-10-29 18:14:53 +0800

    reconstruct the surf_energy in calculator.py

--- gvasp/common/calculator.py
+++ gvasp/common/calculator.py
@@ -1,15 +1,32 @@
-def surface_energy(E_slab, n, E_unit, A):
+from pathlib import Path
+
+from gvasp.common.file import OUTCAR
+
+
+def surface_energy(crystal_dir: str, slab_dir: str):
     """
     Calculate the surface energy
 
-    @params
-        E_slab:		total energy of slab
-        E_unit:		total energy of one structure unit
-        n:			number of structure unit
-        A:			area of surface slab
+    Args:
+        crystal_dir (str): crystal calculation job directory
+        slab_dir (str): slab calculation job directory
 
-    @return
+    Returns:
         E_surf:		surface energy, unit: J/m^2
     """
-    E_surf = (E_slab - n * E_unit) / (2 * A) * 16.02
+    crystal_outcar = OUTCAR(Path(crystal_dir) / "OUTCAR")
+    slab_outcar = OUTCAR(Path(slab_dir) / "OUTCAR")
+
+    E_crystal = crystal_outcar.last_energy
+    E_slab = slab_outcar.last_energy
+
+    N_crystal = crystal_outcar.element
+    N_slab = slab_outcar.element
+
+    lattice_slab = slab_outcar.lattice
+
+    n = N_slab / N_crystal
+    A = lattice_slab.length[0] * lattice_slab.length[1]
+
+    E_surf = (E_slab - n * E_crystal) / (2 * A) * 16.02
     return E_surf
