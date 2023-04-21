c6c4250b2554fda524cfa8a7155812e776ba2fc3
Rasic2 <1051987201@qq.com>
2023-04-21 13:48:57 +0800

    update version in docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = 'GVasp'
 copyright = '2022, Hui Zhou'
 author = 'Hui Zhou'
-version = '0.1.1'
-release = '0.1.1'
+version = '0.1.4.alpha'
+release = '0.1.4.alpha'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
