2d91e2fb1cf285b84fc762f5d1f3ac67c20f4e2d
Rasic2 <1051987201@qq.com>
2023-05-18 22:51:42 +0800

    add font (Arial) in workflow by applying the add_font.py

---
+++ tests/add_font.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+import matplotlib
+
+matplotlibrc_path = Path(matplotlib.matplotlib_fname())
+ttf_path = matplotlibrc_path.parent / "fonts" / "ttf"
+Arial_path = Path('.').absolute() / "tests" / "Arial.ttf"
+os.system(f"cp {Arial_path} {ttf_path}")
