cbce0d37550f40bfa35fe9d0c3ddde294a93445e
Rasic2 <1051987201@qq.com>
2023-09-03 22:39:32 +0800

    chore: update to 0.1.5.beta

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = 'GVasp'
 copyright = '2022, Hui Zhou'
 author = 'Hui Zhou'
-version = '0.1.5.alpha'
-release = '0.1.5.alpha'
+version = '0.1.5.beta'
+release = '0.1.5.beta'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
