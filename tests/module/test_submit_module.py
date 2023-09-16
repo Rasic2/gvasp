245fce33fa52cbe7734a58807cbad3c7d1319681
Rasic2 <1051987201@qq.com>
2023-09-16 21:46:32 +0800

    feat: add support for module in *.submit template

---
+++ tests/module/test_submit_module.py
@@ -0,0 +1,19 @@
+import pytest
+
+from gvasp.common.task import OptTask
+
+
+@pytest.fixture()
+def change_test_dir(request, monkeypatch):
+    monkeypatch.chdir(request.fspath.dirname)
+
+
+class TestSubmitMag(object):
+
+    def test_opt_mag(self, change_test_dir):
+        task = OptTask()
+        task.generate()
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
